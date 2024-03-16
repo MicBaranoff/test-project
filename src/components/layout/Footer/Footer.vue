1c678f69d248d19c1cbae3e3899dfaa5124d38b3
miselbaranov <mb@develup.pro>
2024-03-16 18:43:51 +0200

    feat: footer desktop

---
+++ src/components/layout/Footer/Footer.vue
@@ -0,0 +1,71 @@
+<template>
+  <footer class="footer">
+    <div class="container footer__container">
+      <div class="footer__info">
+        <a href="/" class="footer__logo">
+          <Icon name="logo-dark" />
+        </a>
+        <div class="footer__info-text">
+          <p class="text text--md footer__font footer__font--text">
+            Social media validation business model canvas graphical
+            user interface launch party creative facebook iPad twitter.
+          </p>
+        </div>
+        <p class="text text--md footer__font footer__font--text">All rights reserved.</p>
+      </div>
+
+      <nav class="footer__nav">
+        <div class="footer__nav-col">
+          <span class="text text--lg footer__nav-title footer__font footer__font--title">Landings</span>
+          <ul class="footer__nav-list">
+            <li class="footer__nav-item">
+              <a class="text text--lg footer__font--link footer__nav-link" href="/">Home</a>
+            </li>
+            <li class="footer__nav-item">
+              <a class="text text--lg footer__font--link footer__nav-link" href="/">Products</a>
+            </li>
+            <li class="footer__nav-item">
+              <a class="text text--lg footer__font--link footer__nav-link" href="/">Services</a>
+            </li>
+          </ul>
+        </div>
+        <div class="footer__nav-col">
+          <span class="ext text--lg footer__nav-title footer__font footer__font--title">Company</span>
+          <ul class="footer__nav-list">
+            <li class="footer__nav-item">
+              <a class="text text--lg footer__font--link footer__nav-link" href="/">Home</a>
+            </li>
+            <li class="footer__nav-item">
+              <a class="text text--lg footer__font--link footer__nav-link footer__nav-link--hiring" href="/">Careers</a>
+            </li>
+            <li class="footer__nav-item">
+              <a class="text text--lg footer__font--link footer__nav-link" href="/">Services</a>
+            </li>
+          </ul>
+        </div>
+        <div class="footer__nav-col">
+          <span class="ext text--lg footer__nav-title footer__font footer__font--title">Resources</span>
+          <ul class="footer__nav-list">
+            <li class="footer__nav-item">
+              <a class="text text--lg footer__font--link footer__nav-link" href="/">Blog</a>
+            </li>
+            <li class="footer__nav-item">
+              <a class="text text--lg footer__font--link footer__nav-link" href="/">Products</a>
+            </li>
+            <li class="footer__nav-item">
+              <a class="text text--lg footer__font--link footer__nav-link" href="/">Services</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "Footer"
+}
+</script>
+
+<style scoped lang="scss" src="./footer.scss"></style>
--- src/layouts/DefaultLayout/DefaultLayout.vue
+++ src/layouts/DefaultLayout/DefaultLayout.vue
@@ -2,15 +2,18 @@
   <div class="default-layout">
     <Header class="default-layout__header" />
     <slot></slot>
+    <Footer class="default-layout__footer"></Footer>
   </div>
 </template>
 
 <script>
 import Header from "@/components/layout/Header/Header.vue";
+import Footer from "@/components/layout/Footer/Footer.vue";
 export default {
   name: "DefaultLayout",
   components: {
     Header,
+    Footer,
   }
 }
 </script>
