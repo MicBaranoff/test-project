35f1e36bc0ce549b02066c5dada3906b99791558
miselbaranov <mb@develup.pro>
2024-03-17 18:39:46 +0200

    deploy: dist created

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,10 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
+
+  publicPath: process.env.NODE_ENV === 'production'
+      ? '/test-project/'
+      : '/',
+
   transpileDependencies: true,
 
   css: {
